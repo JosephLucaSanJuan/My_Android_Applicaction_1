7f6bf74dcb79245f8efe189cedaffbee37cab3fd
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-09-24 12:43:42 +0200

    Primera-version

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication1
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.saluda_button)
+        // Alternative: val button: Button = findViewById(R.id.saluda_button)
+        button.setOnClickListener {
+            val name = findViewById<TextInputEditText>(R.id.nameFieldLabel).text.toString()
+            Snackbar.make(button, "Hola $name", Snackbar.LENGTH_LONG).show()
+        }
+    }
+}
